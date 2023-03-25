472c26bcc77ee11acf93d3c33ca5f78fa47b55f3
adambakurecz <adambakurecz@gmail.com>
2023-03-25 21:38:29 +0100

    dark theme - major changes

--- website/logic/header.js
+++ website/logic/header.js
@@ -1,3 +1,4 @@
+//Smaller header
 window.onscroll = function() {
   const header = document.getElementById("header");
   if (window.pageYOffset > 0) {
@@ -6,3 +7,14 @@ window.onscroll = function() {
   }
   header.classList.remove("smaller-header");
 };
+
+//Hamburger menu
+const hamburgerOpen = document.querySelector('.hamburger-open');
+const hamburgerClose = document.querySelector('.hamburger-close');
+const navLinks = document.querySelector('.nav-links');
+hamburgerOpen.addEventListener('click', () => {
+  navLinks.classList.add('active');
+});
+hamburgerClose.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+});
